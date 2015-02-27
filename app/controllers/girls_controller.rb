d1c09e019df914d74612aa4591b81332418fe5f4
Benjamin Winkler <abwinkler999@gmail.com>
2015-02-26 21:23:13 -0500

    Added session choosing

--- app/controllers/girls_controller.rb
+++ app/controllers/girls_controller.rb
@@ -8,6 +8,10 @@ class GirlsController < ApplicationController
 		@girl.user = current_user
 		@girl.level = Level.find_by(id:params[:level_id])
 		@girl.tshirt = Tshirt.find_by(id:params[:tshirt_id])
+		@girl.session_A = Session.find_by(id:params[:session_A_id])
+		@girl.session_B = Session.find_by(id:params[:session_B_id])
+		@girl.session_C = Session.find_by(id:params[:session_C_id])
+		@girl.session_D = Session.find_by(id:params[:session_D_id])
 	    if @girl.save
 	      redirect_to :root
 	    else
@@ -24,6 +28,10 @@ class GirlsController < ApplicationController
 		@girl.user = current_user
 		@girl.level = Level.find_by(id:params[:level_id])
 		@girl.tshirt = Tshirt.find_by(id:params[:tshirt_id])
+		@girl.session_A = Session.find_by(id:params[:session_A_id])
+		@girl.session_B = Session.find_by(id:params[:session_B_id])
+		@girl.session_C = Session.find_by(id:params[:session_C_id])
+		@girl.session_D = Session.find_by(id:params[:session_D_id])
 		# => binding.pry
 	    if @girl.update_attributes(girl_params)
 	      redirect_to :root
@@ -41,6 +49,6 @@ class GirlsController < ApplicationController
 	private
 
 	def girl_params
-		params.require(:girl).permit(:first_name, :last_name, :level_id, :dob, :fallgrade, :troopnumber, :tshirt_id, :dietary, :no_gluten, :medications, :parentcell, :othercell)
+		params.require(:girl).permit(:first_name, :last_name, :level_id, :dob, :fallgrade, :troopnumber, :tshirt_id, :dietary, :no_gluten, :medications, :parentcell, :othercell, :high_ropes, :session_A, :session_B, :session_C, :session_D)
 	end
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,3 +1,4 @@
 class Session < ActiveRecord::Base
 	has_many :girls
+	belongs_to :slot
 end
