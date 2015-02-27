5d80e85081c546a3d308254d253f6195219f5e31
Benjamin Winkler <abwinkler999@gmail.com>
2015-02-27 12:25:25 -0500

    Typo in mailer

--- app/mailers/signup_mailer.rb
+++ app/mailers/signup_mailer.rb
@@ -4,4 +4,5 @@ class SignupMailer < ActionMailer::Base
   def welcome_email(user)
   	@user = current_user
   	mail(to: @user.email, subject: "Registering for AHG Summer Camp Marengo 2015")
+  end
 end
