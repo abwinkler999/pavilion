96974c10a1885feef7dfed1cdb76d0b5dc59aa9b
Benjamin Winkler <abwinkler999@gmail.com>
2016-01-01 00:18:47 -0500

    Incidentals revision

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,9 @@ Slot.find_or_create_by(name: "B")
 Slot.find_or_create_by(name: "C")
 Slot.find_or_create_by(name: "D")
 
-Level.find_or_create_by(name: "Cub")
-Level.find_or_create_by(name: "Puma")
-Level.find_or_create_by(name: "Wildcat")
-Level.find_or_create_by(name: "Lion")
+Level.find_or_create_by(name: "Tenderheart")
+Level.find_or_create_by(name: "Explorer")
+Level.find_or_create_by(name: "Pioneer/Patriot")
 
 Restriction.find_or_create_by(name: "All")
 Restriction.find_or_create_by(name: "Lion Only")
@@ -73,6 +72,6 @@ Adult.find_or_create_by(first_name: "Wally", last_name: "West", address: "1 Sout
 Adult.find_or_create_by(first_name: "Selina", last_name: "Kyle", address: "89 Castle Ave, Gotham City, NH 10200", phone: "614-555-5557", cpr: true, first_aid:false, tshirt: AdultDoubleLarge, troopnumber: "OH2999", troopcontact: "Iris West", dietary: "Kosher", no_gluten: false)
 
 SomeParent = User.last
-Girl.find_or_create_by(first_name: "Meg", last_name: "March", dob: "01/19/2007", level: Puma, dietary: "none", fallgrade: "4", parentcell: "614-555-1020", troopnumber: "OH3030", tshirt: GirlMedium, session_A: ASessions.first, session_B: BSessions.last, session_C: CSessions.first, session_D: DSessions.last, cooking_session:"None", high_ropes:true)
-Girl.find_or_create_by(first_name: "Anne", last_name: "Shirley", dob: "01/19/2006", level: Lion, dietary: "none", fallgrade: "5", parentcell: "614-555-1011", troopnumber: "OH3030", tshirt: GirlMedium, session_A: ASessions.last, session_B: BSessions.first, session_C: CSessions.second, session_D: DSessions.last, cooking_session:"None", high_ropes:false)
-Girl.find_or_create_by(first_name: "Marianne", last_name: "Dashwood", dob: "01/19/2009", level: Cub, dietary: "none", no_gluten:true, fallgrade: "3", parentcell: "614-565-9019", troopnumber: "OH3030", tshirt: GirlMedium, session_A: ASessions.last, session_B: BSessions.first, session_C: CSessions.second, session_D: DSessions.last, cooking_session:"None", high_ropes:false)
+Girl.find_or_create_by(first_name: "Meg", last_name: "March", dob: "01/19/2007", level: Puma, dietary: "none", fallgrade: "4", parentcell: "614-555-1020", troopnumber: "OH3030", tshirt: GirlMedium, session_A: ASessions.first, session_B: BSessions.last, session_C: CSessions.first, session_D: DSessions.last)
+Girl.find_or_create_by(first_name: "Anne", last_name: "Shirley", dob: "01/19/2006", level: Lion, dietary: "none", fallgrade: "5", parentcell: "614-555-1011", troopnumber: "OH3030", tshirt: GirlMedium, session_A: ASessions.last, session_B: BSessions.first, session_C: CSessions.second, session_D: DSessions.last)
+Girl.find_or_create_by(first_name: "Marianne", last_name: "Dashwood", dob: "01/19/2009", level: Cub, dietary: "none", no_gluten:true, fallgrade: "3", parentcell: "614-565-9019", troopnumber: "OH3030", tshirt: GirlMedium, session_A: ASessions.last, session_B: BSessions.first, session_C: CSessions.second, session_D: DSessions.last)
