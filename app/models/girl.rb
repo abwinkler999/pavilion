471f92cb2c841189ce3df7c446bda24ecd8172fe
Benjamin Winkler <abwinkler999@gmail.com>
2016-01-14 19:11:54 -0500

    Regex installed to validate troop number

--- app/models/girl.rb
+++ app/models/girl.rb
@@ -5,4 +5,5 @@ class Girl < ActiveRecord::Base
 	has_and_belongs_to_many :sessions
 
 	validates_presence_of :first_name, :last_name, :level_id, :dob, :fallgrade, :troopnumber, :tshirt_id
+	validates :troopnumber, format: { with: /[A-Z][A-Z]\d+/, message: "Please give full AHG troop name, like 'OH0001'"}
 end
