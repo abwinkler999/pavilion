9a08fd2a94536e70f87512f1fe8ff775d871b7ea
Benjamin Winkler <abwinkler999@gmail.com>
2016-02-15 19:36:33 -0500

    Twenty Tenderheart Limit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,13 @@
 module ApplicationHelper
-	
+
 	def verify_owner(girl)
 		if girl.user != current_user
 		redirect_to :root
 		end
+	end
+
+	def at_tenderheart_limit
+		tenderheart = Level.where(name:"Tenderheart").first
+		return Girl.where(level:tenderheart).count >= 20
 	end
 end
