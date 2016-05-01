0c962a84a27ba09edd2ea296c83af31450b9c4e3
Benjamin Winkler <abwinkler999@gmail.com>
2016-05-01 13:52:25 -0400

    Fixed sessions report

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,40 +5,53 @@ class StaticPagesController < ApplicationController
 
   def special
   end
-  
+
   def admin
-	bounce_nonadmin
+  	bounce_nonadmin
   end
 
   def csv_girls
-	bounce_nonadmin
-	@girls = Girl.all
-	respond_to do |format|
-		format.html
-		format.csv do
-			headers['Content-Disposition'] = "attachment; filename=\"girl-registrations-list.csv\""
-			headers['Content-Type'] ||= 'text/csv'
-		end
-	end
+  	bounce_nonadmin
+  	@girls = Girl.all
+  	respond_to do |format|
+  		format.html
+  		format.csv {
+  			headers['Content-Disposition'] = 'attachment; filename=\"girl-registrations-list.csv\"'
+  			headers['Content-Type'] ||= 'text/csv'
+  		}
+  	end
   end
 
-def csv_adults
-	bounce_nonadmin
-	@adults = Adult.all
-	respond_to do |format|
-		format.html
-		format.csv do
-			headers['Content-Disposition'] = "attachment; filename=\"adult-registrations-list.csv\""
-			headers['Content-Type'] ||= 'text/csv'
-		end
-	end
+  def csv_adults
+  	bounce_nonadmin
+  	@adults = Adult.all
+  	respond_to do |format|
+  		format.html
+  		format.csv {
+  			headers['Content-Disposition'] = 'attachment; filename=\"adult-registrations-list.csv\"'
+  			headers['Content-Type'] ||= 'text/csv'
+  		}
+    end
   end
-end
+
+  def csv_sessions
+  	bounce_nonadmin
+  	@girls = Girl.all
+  	respond_to do |format|
+  		format.html
+  		format.csv {
+  			headers['Content-Disposition'] = 'attachment; filename=\"adult-registrations-list.csv\"'
+  			headers['Content-Type'] ||= 'text/csv'
+  		}
+    end
+  end
 
 private
 
-def bounce_nonadmin
-	unless user_signed_in? && current_user.is_admin?
-		redirect_to action: "home"
-	end
+  def bounce_nonadmin
+  	unless user_signed_in? && current_user.is_admin?
+  		redirect_to action: "home"
+  	end
+  end
+
 end
